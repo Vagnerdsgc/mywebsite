46b522a62e071b4f66b8cceba84204693fb5c45c
Vagner Silva <vagnerds@hotmail.com>
2026-09-09 20:17:52 -0300

    H1 para h2

--- Python/Primeiro.py
+++ Python/Primeiro.py
@@ -1,1 +1,10 @@
-for i 1 to 10
+nota1=5
+nota2=7.5
+media = (nota1 + nota2)/2
+Nome = 'Vagner'
+print ("Nome " + Nome)
+print ("Primeira nota %.2f \nSegunda nota %.2f" %(nota1 , nota2))
+print ("Media = ", media)
+nome = input ("Qual o seu nome: ")
+idade = int(input("Qual a sua idade: "))
+print ("Media ao quadrado %.2f" %(media**2))
---
+++ Python/idade.py
@@ -0,0 +1,4 @@
+lista = ["Ana", "João"]
+
+for i, nome in enumerate(lista):
+    print(i, nome)
